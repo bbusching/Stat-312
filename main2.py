fa8105f25367cdfe38453073437c675c60afbf4a
Brad Busching <bradley.busching@gmail.com>
2014-12-03 18:41:59 -0800

    organized second set of data for minitab

--- main2.py
+++ main2.py
@@ -36,34 +36,47 @@ def main():
   for file in genesis.fileids():
     for word in genesis.words(file):
       genesis_word_lens.append(len(word))
-
-  with open("results2.txt", 'w') as f:
+  with open("wordlens.txt", 'w') as f:
     sys.stdout = f
-    f.write("GENESIS\n")
-    f.write("---------------------\n")
-    for obs in genesis_word_lens:
-      f.write(str(obs) + " ")
-    f.write("\n\n\n")
-    f.write("GUTENBERG\n")
-    f.write("---------------------\n")
-    for obs in gutenberg_word_lens:
-      f.write(str(obs) + " ")
-    f.write("\n\n\n")
-    f.write("WEBTEXT\n")
-    f.write("---------------------\n")
-    for obs in web_word_lens:
-      f.write(str(obs) + " ")
-    f.write("\n\n\n")
-    f.write("INAUGURAL\n")
-    f.write("---------------------\n")
-    for obs in inaugural_word_lens:
-      f.write(str(obs) + " ")
-    f.write("\n\n\n")
-    f.write("BROWN\n")
-    f.write("---------------------\n")
-    for obs in brown_word_lens:
-      f.write(str(obs) + " ")
-    f.write("\n\n\n")
+    f.write("GENESIS, INAUGURAL, WEBTEXT, BROWN, GUTENBERG\n")
+    for i in xrange(max(len(genesis_word_lens), len(inaugural_word_lens), 
+        len(web_word_lens), len(brown_word_lens),
+        len(gutenberg_word_lens))):
+      for corpus in [genesis_word_lens, inaugural_word_lens,
+          web_word_lens, brown_word_lens, gutenberg_word_lens]:
+        if(i >= len(corpus)):
+          f.write(",")
+        else:
+          f.write(str(corpus[i]) + ",")
+      f.write("\n")
+#
+ # with open("results2.txt", 'w') as f:
+ #   sys.stdout = f
+  #  f.write("GENESIS\n")
+ #   f.write("---------------------\n")
+ #   for obs in genesis_word_lens:
+ #     f.write(str(obs) + " ")
+ #   f.write("\n\n\n")
+ #   f.write("GUTENBERG\n")
+ #   f.write("---------------------\n")
+ #   for obs in gutenberg_word_lens:
+ #     f.write(str(obs) + " ")
+ #   f.write("\n\n\n")
+ #   f.write("WEBTEXT\n")
+ #   f.write("---------------------\n")
+ #   for obs in web_word_lens:
+ #     f.write(str(obs) + " ")
+ #   f.write("\n\n\n")
+ #   f.write("INAUGURAL\n")
+ #   f.write("---------------------\n")
+ #   for obs in inaugural_word_lens:
+ #     f.write(str(obs) + " ")
+ #   f.write("\n\n\n")
+ #   f.write("BROWN\n")
+ #   f.write("---------------------\n")
+ #   for obs in brown_word_lens:
+  #    f.write(str(obs) + " ")
+  #  f.write("\n\n\n")
 
 
 if __name__ == "__main__":
